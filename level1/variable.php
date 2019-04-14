0ea0d2c6a617ca81fd9f750055acce5796ff51ec
Andy Maulana Yusuf <goingtoprofandy@gmail.com>
2019-04-15 00:29:04 +0700

    Level 1 | Variables
    1. Add Description
    2. Add Source Content
    3. Add Example Code
    
    #Complete

---
+++ level1/variable.php
@@ -0,0 +1,41 @@
+<?php 
+// Ini syntax pembukan php
+
+/**
+ * PENJELASAN VARIABEL
+ * Variabel merupakan suatu tempat dimemory yang digunakan oleh bahasa pemograman
+ * untuk menyimpan data sementara selama runtime suatu program berjalan.
+ * Variabel sendiri bersifat sementara, dan tidak tetap sehingga nilainya dapat
+ * berubah ubah.
+ * 
+ * Dalam PHP variabel disimbolkan dengan tanda berawalan $ yang disertai nama dari 
+ * variabel yang akan dibuat, dan juga nilai dari variabel tersebut.
+ * Sama seperi bahasa pemograman lain, di PHP variabel memiliki 2 siklus sebelum dapat digunakan
+ * yaitu deklarasi dan inisialisasi.
+ * 
+ * Dalam PHP terdapat aturan dalam menggunakan variabel yaitu sebagai berikut
+ * 1. Variabel harus selalu diawali dengan tanda $ lalu disertai nama dari variabelnya
+ * 2. Nama variabel harus diawali dengan huruf atau karakter underscore _
+ * 3. Nama variabel tidak boleh diawali dengan nomor
+ * 4. Nava variabel bersifat case-sensitive, yang artinya membedakan huruf besar dan kecil.
+ * 
+ * Deklarasi merupakan suatu siklus dimana suatu variabel itu diperkenalkan
+ * sebagai contoh $text;
+ * yang artinya variabel $text telah dibuat.
+ * 
+ * Inisialisasi merupakan suatu siklus dimana suatu variabel itu ditentukan nilainya
+ * sebagai contoh $text = "Example Text";
+ * yang artinya variabel $text telah dibuat, dan berisikan "Example Text".
+ * 
+ * untuk lebih lengkapnya silahkan lihat contoh dibawah ini.
+ * 
+ * sumber: https://www.w3schools.com/php7/php7_variables.asp
+ */
+
+// Contoh Variabel
+$number = 10;
+$condition = false;
+$text = "Example Text";
+
+// Ini syntax penutup php
+?>
